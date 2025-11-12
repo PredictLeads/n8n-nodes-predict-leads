cae23c21a3f92f9a80c98c396f8fc0dd2fa8aac1
Dean Koštomaj <dean.kostomaj@predictleads.com>
2025-11-12 13:05:58 +0100

    Improve sizes query param for Companies Endpoint

--- nodes/PredictLeads/descriptions/RetrieveCompaniesDescription.ts
+++ nodes/PredictLeads/descriptions/RetrieveCompaniesDescription.ts
@@ -14,7 +14,8 @@ export const retrieveCompaniesOperation: INodePropertyOptions = {
 				sizes: '={{ $parameter.sizes }}',
 				page: '={{ $parameter.page }}',
 				limit: '={{ $parameter.limit }}',
-			}
+			},
+			arrayFormat: 'brackets',
 		},
 	},
 };
@@ -24,14 +25,52 @@ export const retrieveCompaniesFields: INodeProperties[] = [
 		// TODO: Sizes should be an array
 		displayName: 'Sizes',
 		name: 'sizes',
-		type: 'string',
+		type: 'multiOptions',
 		required: true,
 		displayOptions: {
 			show: {
 				operation: ['retrieveCompanies'],
 			},
 		},
-		default: '2-10',
+		options: [
+			{
+				name: '1',
+				value: '1',
+			},
+			{
+				name: '2-10',
+				value: '2-10',
+			},
+			{
+				name: '11-50',
+				value: '11-50',
+			},
+			{
+				name: '51-200',
+				value: '51-200',
+			},
+			{
+				name: '201-500',
+				value: '201-500',
+			},
+			{
+				name: '501-1,000',
+				value: '501-1000',
+			},
+			{
+				name: '1,001-5,000',
+				value: '1001-5000',
+			},
+			{
+				name: '5,001-10,000',
+				value: '5001-10000',
+			},
+			{
+				name: '10,000+',
+				value: '10,000+',
+			},
+		],
+		default: '',
 		description: 'An array of one or more valid company sizes.',
 	},
 ];
